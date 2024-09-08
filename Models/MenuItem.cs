1ab8a8a4837c84f24e6cacab161d571589d287fa
XuShiTa <namlun96@gmail.com>
2024-09-09 01:03:05 +0700

    Lab 2

---
+++ Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace UTC_ASP.NET_Web_Lab.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
